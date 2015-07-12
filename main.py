25e90cf4b81985d8579b4b8edadbcba99f278b2c
Kartik Luke Singh <kartikluke@gmail.com>
2015-07-12 12:55:29 +0530

    started on bot api

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
+import requests
 from flask import Flask
+from flask import request
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
 # Note: We don't need to call run() since our application is embedded within
 # the App Engine WSGI application server.
 
+def bot_api(method_name, method, params = {}):
+    path = "https://api.telegram.org/bot91678886:AAHpiQP0tCIfeqXKy5zA8BEPHOm7EZOkLCU/" + method_name
+    if params
+        path += "?"
+        for param in params
+            path += param + "=" + params[param]
+    if method == 'GET'
+        return requests.get(path, verify=True).content
+    if method == 'POST'
+        return requests.post(path, verify=True).content
 
 @app.route('/')
 def hello():
     """Return a friendly HTTP greeting."""
     return 'Hello World!'
 
+@app.route('/91678886:AAHpiQP0tCIfeqXKy5zA8BEPHOm7EZOkLCU', methods=['POST'])
+def bot():
+    """Get bot."""
+    return bot_api('getMe', 'GET'), 200
 
 @app.errorhandler(404)
 def page_not_found(e):
